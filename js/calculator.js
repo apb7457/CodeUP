16a01b24d90c418bcf8173593a61e53f788c4efe
andrewblouet <andrewblouet@hotmail.com>
2017-08-30 10:37:23 -0500

    'Bliss' is the place for upscale, yet downtoned, eats.  En estilo nueva sud-oeste americano o 'Nouveau Sud-Ouest Americane'-En FranÇais....Muy Cerca de mi casa: Reiver Cottage....

---
+++ js/calculator.js
@@ -0,0 +1,52 @@
+var btns = document.getElementsByClassName("calcButton");
+var leftDisplay = document.getElementById("calcDisplay1");
+var signBtns = document.getElementsByClassName("calcSignButton");
+var centerDisplay = document.getElementById("calcDisplay2");
+var rightDisplay = document.getElementById("calcDisplay3");
+var equalsSign = document.getElementById("equals")
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+
+        if (centerDisplay.getAttribute("value") !== "" && leftDisplay.getAttribute("value") !== "") {
+            var value = rightDisplay.getAttribute("value");
+            value += this.getAttribute("value");
+            rightDisplay.setAttribute("value", value);
+        } else {
+            var value = leftDisplay.getAttribute("value");
+            value = value + this.getAttribute("value");
+
+            leftDisplay.setAttribute("value", value);
+        }
+    })
+}
+
+for (var i = 0; i < signBtns.length; i++) {
+    signBtns[i].addEventListener("click", function () {
+        centerDisplay.setAttribute("value", this.getAttribute("value"));
+    })
+}
+equalsSign.addEventListener("click", function () {
+    var centerValue = centerDisplay.getAttribute("value");
+    var leftValue = parseInt(leftDisplay.getAttribute("value"));
+    var rightValue = parseInt(rightDisplay.getAttribute("value"));
+    var total;
+
+    switch (centerValue) {
+        case "+":
+            total = leftValue + rightValue;
+            break;
+        case "-":
+            total = leftValue - rightValue;
+            break;
+        case "/":
+            total = leftValue / rightValue;
+            break;
+        case "x":
+            total = leftValue * rightValue;
+            break;
+    }
+    leftDisplay.setAttribute("value", total);
+    centerDisplay.setAttribute("value", "");
+    rightDisplay.setAttribute("value", "");
+
+})
